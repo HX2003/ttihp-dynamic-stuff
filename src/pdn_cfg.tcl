23bb117f12cea2b405a5b3b56e3a84ebf4dde7f1
HX2003 <HX2003@users.noreply.github.com>
2026-03-29 02:02:36 +0800

    tweak

--- src/pdn_cfg.tcl
+++ src/pdn_cfg.tcl
@@ -5,17 +5,10 @@
 source $::env(SCRIPTS_DIR)/openroad/common/set_global_connections.tcl
 set_global_connections
 
-# 1. Map the physical pin names (VPWR/VGND) to the logical net names (VDD/VSS)
-# This ensures that when the tool sees a pin named 'VPWR', it treats it as 'VDD'.
-add_global_connection -net $::env(VDD_NET) -pin_pattern {^VPWR$} -power
-add_global_connection -net $::env(GND_NET) -pin_pattern {^VGND$} -ground
-
-set_voltage_domain -name CORE -power $::env(VDD_NET) -ground $::env(GND_NET)
-
 # 2. Define a "dummy" macro grid 
 # We need this because 'add_pdn_connect' must be associated with a grid name.
 # We don't add any stripes to it; it just serves as a container for the connection rule.
-define_pdn_grid -macro -default -name macro_stitch -voltage_domains {CORE}
+define_pdn_grid -macro -default -name macro_stitch -starts_with POWER
 
 # 3. The Only Functional Command: Drop Vias
 # This will automatically place Via3 anywhere Metal 3 (pins) and Metal 4 (stripes) overlap.
