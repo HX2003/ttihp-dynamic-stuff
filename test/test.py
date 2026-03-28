1e6ce3c7563dba494d616ac716358514b70cfe75
HX2003 <HX2003@users.noreply.github.com>
2026-03-28 23:20:30 +0800

    fix next steps

---
+++ test/test.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
+ 
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
